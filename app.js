f418148c1e0e51d3bab8c2ed43f78f96666d8804
Anthony Ynami <Anthony Ynami>
2017-05-08 20:02:11 -0400

    werd

--- app.js
+++ app.js
@@ -1,10 +1,40 @@
-var express = require('express')
-var app = express()
 
-app.get('/', function (req, res) {
-  res.send('Hello World!')
-})
+var express = require('express');
+var handlebars = require('express-handlebars')
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!')
-})
+var app = express();
+
+function processData(data) {
+ // taking care of data
+		}
+
+function handler() {
+  if(this.status == 200 &&
+   	 this.responseXML != null &&
+    this.responseXML.getElementById('test').textContent) {
+    // success!
+    processData(this.responseXML.getElementById('test').textContent);
+} else {
+    // something went wrong
+  }
+}
+
+var client = new XMLHttpRequest();
+client.onload = handler;
+apikey="c8b3b1c0f05958773f39d19134e7a7d8";
+client.open("GET", "https://api.musixmatch.com/ws/1.1/");
+client.send();
+
+
+app.engine('handlebars', handlebars({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+
+app.use('/static', express.static('public'))
+
+app.get('/', function(req, res) {
+  res.render('home');
+});
+
+app.get('/users/:username', function(req, res) {
+  res.render('user', {name: req.params.username});
+});
